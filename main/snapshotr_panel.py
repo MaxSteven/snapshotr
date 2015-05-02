4f9112d6ca245fd86965e161ae65a6341000e0f1
Andrew Savchenko <art@artaman.net>
2015-05-02 21:32:18 +0800

    Moved ssPanel to separate file

--- main/snapshotr_panel.py
+++ main/snapshotr_panel.py
@@ -1,20 +1,3 @@
-# -*- coding: utf-8 -*-
-# --------------------! DEV BRANCH !----------------------------
-# Snapshotr: Nuke snapshots manager
-#
-# Andrew Savchenko © 2014
-# art@artaman.net
-#
-# Attribution 4.0 International (CC BY 4.0)
-# http://creativecommons.org/licenses/by/4.0/
-#
-# Developed on OS X and RHEL, should work on random *nix system
-#
-# --------------------! DEV BRANCH !----------------------------
-
-__version__ = "0.1.9"
-__release__ = False
-
 import nuke
 import nukescripts
 import os
@@ -25,14 +8,12 @@ import threading
 import webbrowser
 from PIL import Image
 from PySide import QtCore
-from sys import path, platform
-
+from sys import path
 snapr_path = os.getenv("HOME") + "/.nuke/snapshotr"
 path.append(snapr_path)
 from main import markup, scandir
 
-if nuke.GUI:
-    class ssPanel(nukescripts.PythonPanel):
+class ssPanel(nukescripts.PythonPanel):
 
         def __init__(self):
             nukescripts.PythonPanel.__init__(
@@ -843,15 +824,3 @@ if nuke.GUI:
 
         def notCatched(self):
             print "\n! Unknown knob changed"
-
-    def addSSpanel():
-        return ssPanel().addToPane()
-else:
-    pass
-
-if nuke.GUI:
-    menu = nuke.menu("Pane")
-    menu.addCommand("Snapshotr", addSSpanel)
-    nukescripts.registerPanel("uk.co.thefoundry.ssPanel", addSSpanel)
-else:
-    print "\n~ Nuke is running in non-gui mode, Snapshotr isn't activated"
